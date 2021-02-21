261ead34002bd5176e5bcf417daa9e07be8915ce
Аймкулова Ботагоз <botagoz.aimkulova@gmail.com>
2021-02-21 20:00:09 +0600

    #3 На экране выводится цифры по нажатию на кнопки

--- src/containers/Buttons/Buttons.js
+++ src/containers/Buttons/Buttons.js
@@ -1,24 +1,30 @@
 import "./Buttons.css";
 import { Button } from "reactstrap";
+import { useDispatch } from "react-redux";
+import { ADD_NUMBER } from '../../store/actionTypes';
 
 const Buttons = () => {
-  
+    const dispatch = useDispatch();
+    const addNumber = number => {
+        dispatch({type: ADD_NUMBER, value: number})
+    }
+
   return (
     <div className='Buttons'>
-      <Button color='secondary'>7</Button>
-      <Button color='secondary'>8</Button>
-      <Button color='secondary'>9</Button>
+      <Button color='secondary' onClick={() => addNumber(7)}>7</Button>
+      <Button color='secondary' onClick={() => addNumber(8)}>8</Button>
+      <Button color='secondary' onClick={() => addNumber(9)}>9</Button>
       <Button color='primary'>+</Button>
-      <Button color='secondary'>4</Button>
-      <Button color='secondary'>5</Button>
-      <Button color='secondary'>6</Button>
+      <Button color='secondary' onClick={() => addNumber(4)}>4</Button>
+      <Button color='secondary' onClick={() => addNumber(5)}>5</Button>
+      <Button color='secondary' onClick={() => addNumber(6)}>6</Button>
       <Button color='primary'>&ndash;</Button>
-      <Button color='secondary'>3</Button>
-      <Button color='secondary'>2</Button>
-      <Button color='secondary'>1</Button>
+      <Button color='secondary' onClick={() => addNumber(3)}>3</Button>
+      <Button color='secondary' onClick={() => addNumber(2)}>2</Button>
+      <Button color='secondary' onClick={() => addNumber(1)}>1</Button>
       <Button color='primary'>*</Button>
       <Button color='danger'> &lt; </Button>
-      <Button color='secondary'>0</Button>
+      <Button color='secondary' onClick={() => addNumber(0)}>0</Button>
       <Button color='success'>=</Button>
       <Button color='primary'>/</Button>
     </div>
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,13 @@
+import { ADD_NUMBER } from './actionTypes';
+
 const initialState = {
-    counter: 0
+    counter: '0'
 };
 
 const reducer = (state = initialState, action) => {
+    if(action.type === ADD_NUMBER) {
+return {...state, counter: state.counter + action.value}
+    }
     return state;
 };
 
