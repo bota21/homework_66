0c79ff19bbac76dd77546ddcafa9d82e682328e4
Аймкулова Ботагоз <botagoz.aimkulova@gmail.com>
2021-02-21 19:40:14 +0600

    #3 На экране показывает значение стейта

--- src/containers/Display/Display.js
+++ src/containers/Display/Display.js
@@ -1,10 +1,12 @@
 import "./Display.css";
+import { useSelector } from 'react-redux';
 
 const Display = () => {
+    const counter = useSelector(state => state.counter);
 
   return (
     <div>
-      <h2 className='Display'>1234567890</h2>
+      <h2 className='Display'>{counter}</h2>
     </div>
   );
 };
