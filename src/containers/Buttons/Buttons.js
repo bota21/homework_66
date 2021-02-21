d6be2f3cf0a48947de01290f448c02703335a9a1
Аймкулова Ботагоз <botagoz.aimkulova@gmail.com>
2021-02-21 17:40:31 +0600

    #1 Приложение создано, зависемости добавлены, реализован простой дизайн

---
+++ src/containers/Buttons/Buttons.js
@@ -0,0 +1,27 @@
+import "./Buttons.css";
+import { Button } from "reactstrap";
+
+const Buttons = () => {
+  return (
+    <div className='buttons_wrapper'>
+      <Button color='secondary'>7</Button>
+      <Button color='secondary'>8</Button>
+      <Button color='secondary'>9</Button>
+      <Button color='primary'>+</Button>
+      <Button color='secondary'>4</Button>
+      <Button color='secondary'>5</Button>
+      <Button color='secondary'>6</Button>
+      <Button color='primary'>&ndash;</Button>
+      <Button color='secondary'>3</Button>
+      <Button color='secondary'>2</Button>
+      <Button color='secondary'>1</Button>
+      <Button color='primary'>*</Button>
+      <Button color='danger'> &lt; </Button>
+      <Button color='secondary'>0</Button>
+      <Button color='success'>=</Button>
+      <Button color='primary'>/</Button>
+    </div>
+  );
+};
+
+export default Buttons;
---
+++ src/containers/Display/Display.js
@@ -0,0 +1,11 @@
+import { Input } from "reactstrap";
+
+const Display = () => {
+  return (
+    <div>
+      <Input type='text' />
+    </div>
+  );
+};
+
+export default Display;
